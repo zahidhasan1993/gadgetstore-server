5b1d960af3ded4a2bdedc934583b2fe7acb77c30
devZahid <officialzahidhasan19932021@gmail.com>
2024-01-24 13:08:47 +0600

    adding mongoose and connect to db

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const express = require("express");
-const app = express();
-const port = 3000 || process.env.PORT;
-const products = require("./data/products");
-const cors = require("cors");
+import express from "express";
+import cors from "cors";
+import products from "./data/products.js";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
+
+const port = 3000 || process.env.PORT;
+
+const app = express();
+//middlewares
+app.use(cors());
+dotenv.config();
+
+// DB Connection
+connectDB();
 app.get("/", (req, res) => {
   res.send("GADGETSTORE SERVER IS RUNNING");
 });
@@ -17,5 +27,5 @@ app.get("/api/products/:id", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`app running on port ${port}`);
+  console.log(`app running ${process.env.NODE_STS} on port ${port}`);
 });
